57d47d8efe62d74ebb34b203de6f682e748e7dd0
mumumu <mumumu@znzkj.net.cn>
2022-08-29 09:28:17 +0800

    bug fix

--- room/src/main/java/com/tkm/room/words/WordViewModel.kt
+++ room/src/main/java/com/tkm/room/words/WordViewModel.kt
@@ -1,8 +1,10 @@
 package com.tkm.room.words
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class WordViewModel(private val repository: WordRepository) : ViewModel() {
