fe86239e841364eb97d40ef2bf1365d87a3450e5
mumumu <mumumu@znzkj.net.cn>
2022-09-05 18:26:47 +0800

    Merge branch 'master' of github.com:l793854601/TKMKitDemo
    # Conflicts:
    #	.idea/misc.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/tkm/tkmkitdemo/MainActivity.kt
    #	app/src/main/res/layout/activity_login.xml
    #	app/src/main/res/layout/activity_main.xml
    #	buildSrc/build/classes/kotlin/main/Dependencies.class
    #	buildSrc/build/kotlin/compileKotlin/build-history.bin
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/inputs/source-to-output.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/inputs/source-to-output.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/class-attributes.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/class-attributes.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/class-fq-name-to-source.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/class-fq-name-to-source.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/constants.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/constants.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/internal-name-to-source.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/internal-name-to-source.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/proto.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/proto.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/source-to-classes.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/jvm/kotlin/source-to-classes.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/counters.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/file-to-id.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/file-to-id.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/id-to-file.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/id-to-file.tab.keystream
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/id-to-file.tab.keystream.len
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/id-to-file.tab.len
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/id-to-file.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/id-to-file.tab_i
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/lookups.tab
    #	buildSrc/build/kotlin/compileKotlin/caches-jvm/lookups/lookups.tab.values.at
    #	buildSrc/build/kotlin/compileKotlin/last-build.bin
    #	buildSrc/build/libs/buildSrc.jar
    #	lib_base/build.gradle
    #	lib_base/src/main/java/com/tkm/lib_base/mvvm/BaseViewModel.kt

--- workmanager/src/main/java/com/tkm/workmanager/MainActivity.kt
+++ workmanager/src/main/java/com/tkm/workmanager/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 const val INPUT_DATA_KEY = "INPUT_DATA_KEY"
 const val OUTPUT_DATA_KEY = "OUTPUT_DATA_KEY"
@@ -30,6 +31,9 @@ class MainActivity : AppCompatActivity() {
                 .then(workRequestB)
                 .enqueue()
 
+            //  取消任务
+//            workManager.cancelWorkById(workRequestB.id)
+
             workManager.getWorkInfoByIdLiveData(workRequestA.id).observe(this) { workInfo ->
                 if (workInfo.state == WorkInfo.State.SUCCEEDED) {
                     //  成功
@@ -43,15 +47,21 @@ class MainActivity : AppCompatActivity() {
     private fun createWorkRequest(name: String): OneTimeWorkRequest {
         //  创建约束条件：电量、网络、存储等
         val constraints = Constraints.Builder()
-    //                .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .setRequiredNetworkType(NetworkType.NOT_REQUIRED)
     //                .setRequiresBatteryNotLow(true)
     //                .setRequiresCharging(true)
     //                .setRequiresStorageNotLow(true)
             .build()
         //  创建WorkRequest
         val workRequest = OneTimeWorkRequestBuilder<MyWorker>()
+            //  入参
             .setInputData(workDataOf(INPUT_DATA_KEY to name))
+            //  设置约束
             .setConstraints(constraints)
+            //  延迟执行
+            .setInitialDelay(2, TimeUnit.SECONDS)
+            //  设置退避指数
+            .setBackoffCriteria(BackoffPolicy.LINEAR, 2, TimeUnit.SECONDS)
             .build()
         return workRequest
     }
